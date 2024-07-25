fd6d03f8d6e154239a08514b1917b99769b9e1e6
mikkel agerlin <mikkel_ac@hotmail.com>
2024-07-25 19:34:20 +0200

    fix: padding, layout, sizing

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,19 @@ export default function Home() {
   const frikkeDepnerTargetDate = new Date("2024-09-01T00:00:00Z");
   const mikkelTargetDate = new Date("2024-10-01T00:00:00Z");
 
+  const imageScaling = 0.8;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-32">
-      <h1 className="header">{"✨ It's honkin' time in... ✨"}</h1>
-      <div className={"grid grid-cols-2 items-center gap-8 p-32"}>
+    <main className="flex min-h-screen flex-col content-center items-center gap-4 p-4">
+      <h1 className="header whitespace-nowrap">
+        {"✨ It's honkin' time in... ✨"}
+      </h1>
+      <div className={"grid grid-cols-2 items-center gap-8"}>
         <Image
           src="/goose house.png"
           alt="goose"
-          width={300}
-          height={24}
+          width={300 * imageScaling}
+          height={24 * imageScaling}
           priority
         />
         <Countdown
@@ -24,8 +28,8 @@ export default function Home() {
         <Image
           src="/goose tower.png"
           alt="goose"
-          width={250}
-          height={24}
+          width={250 * imageScaling}
+          height={24 * imageScaling}
           priority
         />
         <Countdown title="🏠 Casa Agerlin" targetDate={mikkelTargetDate} />
